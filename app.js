86cd2262b7d763a567e1a624c32eec06bd2384da
patelkirtan03 <kirtan.patel@shaligraminfotech.com>
2024-04-26 12:34:44 +0530

    added a env file config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5600
+require('dotenv').config()
 app.listen(port, () => {
     console.log('Server listening on 5600')
 });
